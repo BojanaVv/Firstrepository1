d1cef3b4768060b921f5a211ed7bc62fab1401ef
BojanaVv <bvrzovska@gmail.com>
2023-05-15 20:43:04 +0200

    my first commit

---
+++ src/Package2/Tests2.java
@@ -0,0 +1,11 @@
+package Package2;
+
+    public class Tests2{
+
+       public static void main(String [] args) {
+System.out.printf("Hello and welcome!");
+}
+
+    public static void sayHEllo(String name)
+    {System.out.println("Hello" +name);}
+}
